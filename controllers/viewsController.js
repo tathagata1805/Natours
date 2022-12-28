6093e1f944480a93c45bc2b8d50dedba6b2b87f4
tathagata1805 <tathagata1805@gmail.com>
2022-12-28 11:43:27 +0530

    added single tour page, its controller and routes along with review section

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,7 @@
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 // CONTROLLER FUNCTION TO GET THE OVERVIEW PAGE TO VIEW ALL TOURS
 
@@ -7,7 +9,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) GET ALL TOUR DATA FROM COLLECTION
   const tours = await Tour.find();
 
-  // 2) BUILD TEMPLATE IN THE TEMPLATE FILE
+  // 2) BUILD TEMPLATE (IN THE TEMPLATE FILE)
 
   // 3) RENDER THE TEMPLATE USING DATA
   res.status(200).render('overview', {
@@ -17,8 +19,56 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 });
 
 // CONTROLLER FUNCTION TO GET A SINGLE TOUR
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res, next) => {
+  // GET THE DATA FOR THE REQUESTED TOUR (INCLUDING REVIEWS AND GUIDES)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  // IF NO TOUR, SHOW ERROR
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
+  // 2) BUILD THE TEMPLATE (IN THE TEMPLATE FILE)
+
+  // 3) RENDER TEMPLATE USING DATA
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour,
+  });
+});
+
+// GET THE LOGIN FORM TEMPLATE
+exports.getLoginForm = catchAsync(async (req, res, next) => {
+  res.status(200).render('login', {
+    title: 'Login into your Account',
+  });
+});
+
+// GET THE USER ACCOUNT PAGE TEMPLATE
+exports.getAccount = catchAsync(async (req, res, next) => {
+  res.status(200).render('account', {
+    title: 'Your Account',
+  });
+});
+
+// CONTROLLER FUNCTION TO HANDLE USER UPDATE
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.user.name,
+      email: req.user.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(200).render('account', {
+    title: 'Your Account',
+    user: updatedUser,
+  });
+});
