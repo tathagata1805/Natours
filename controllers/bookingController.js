f56e8c726a4979d9e1aa47f28a416071027817c7
tathagata1805 <tathagata1805@gmail.com>
2023-01-05 18:00:50 +0530

    final commit

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,5 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
-// const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
